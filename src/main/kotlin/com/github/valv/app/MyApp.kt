1f10016efddf0967452c4817c405b769bd5ac77d
Vladimir Valeyev <ValV@users.noreply.github.com>
2018-08-21 10:45:55 +0600

    Initial commit

---
+++ src/main/kotlin/com/github/valv/app/MyApp.kt
@@ -0,0 +1,6 @@
+package com.github.valv.app
+
+import com.github.valv.view.MainView
+import tornadofx.*
+
+class MyApp: App(MainView::class, Styles::class)
---
+++ src/main/kotlin/com/github/valv/view/MainView.kt
@@ -0,0 +1,8 @@
+package com.github.valv.view
+
+import javafx.scene.layout.BorderPane
+import tornadofx.*
+
+class MainView : View("IS 2018.6s VVV OOP") {
+    override val root: BorderPane by fxml()
+}
