2e9a43624f68292b570342f8ddb9736fdb805b7d
Vladimir Valeyev <ValV@users.noreply.github.com>
2018-08-22 16:26:44 +0600

    Add drop focus on text input controls

--- src/main/kotlin/com/github/valv/view/MainView.kt
+++ src/main/kotlin/com/github/valv/view/MainView.kt
@@ -1,8 +1,13 @@
 package com.github.valv.view
 
+import javafx.application.Platform
 import javafx.scene.layout.BorderPane
 import tornadofx.*
 
 class MainView : View("IS 2018.6s VVV OOP") {
     override val root: BorderPane by fxml()
+
+    init {
+        Platform.runLater { root.requestFocus() }
+    }
 }
